f639d59aec3c5b92a4d9270c10d77ed55be334b9
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-09-26 21:35:42 -0700

    Created an ActivityDashboard feature that just lists all the activites. Passing in the activity list from App since App will also support other requests like creating an activity.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,14 +1,14 @@
-import { Container, CssBaseline, List, ListItem, ListItemText } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios
-      .get<Activity[]>("https://localhost:5001/api/activities")
+    axios.get<Activity[]>("https://localhost:5001/api/activities")
       .then((response) => setActivities(response.data));
 
     return () => {};
@@ -19,15 +19,8 @@ function App() {
       <CssBaseline/>
       <NavBar />
       <Container maxWidth='xl' sx={{mt: 3}}>
-        <List>
-          {activities.map((activity) => (
-            <ListItem key={activity.id}>
-              <ListItemText>{activity.title}</ListItemText>
-            </ListItem>
-          ))}
-        </List>
-      </Container>
-      
+        <ActivityDashboard activities={activities}/>
+      </Container>    
     </>
   );
 }
