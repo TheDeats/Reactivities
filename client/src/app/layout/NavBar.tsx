ac9c168707b018d88c5d88ad4801dd37b45eefd4
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-13 21:09:43 -0700

    Added a UiStore to have reactions to the page loading. Updated the counterStore with a different approach to mobx observables and added events when the counts are incremented/decremented. Those events are logged in the Counter component. In the NavBar we add an Observer for uiSore.isLoading and display a LinerProgress graphic when the page is loading. Using the agent to handle setting the isLoading property by intercepting all requests and calling uiStore.IsBusy() then calling isIdle in the response interceptor.

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,14 +1,18 @@
 import Group from "@mui/icons-material/Group";
-import {Box, AppBar, Toolbar, Container, MenuItem, Typography} from "@mui/material";
+import {Box, AppBar, Toolbar, Container, MenuItem, Typography, LinearProgress} from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
+import { useStore } from "../../lib/hooks/useStore";
+import { Observer } from "mobx-react-lite";
 
 export default function NavBar() {
+  const {uiStore} = useStore();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
         position="static"
-        sx={{backgroundImage: "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)"}}>
+        sx={{backgroundImage: "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)", position: 'relative'}}>
         <Container>
           <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
             <Box>
@@ -25,6 +29,22 @@ export default function NavBar() {
             <MenuItem>User Menu</MenuItem>
           </Toolbar>
         </Container>
+
+        <Observer>
+          {() => uiStore.isLoading ? (
+            <LinearProgress
+              color="secondary"
+              sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: 4
+              }}
+            />
+          ) : null}
+        </Observer>
+
       </AppBar>
     </Box>
   );
