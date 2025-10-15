068da1fe1da5355d13c2f6f31e99b772d772cc3e
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-15 16:56:22 -0700

    Created a zod schema for client side validation. Implemented in the ActivityForm for the Title field. Tested and working.

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -3,9 +3,14 @@ import { useActivities } from "../../../lib/hooks/useActivities";
 import { useForm, type FieldValues } from 'react-hook-form'
 import { useEffect } from "react";
 import { useParams } from "react-router";
+import type { ActivitySchema } from "../../../lib/schemas/activitySchema";
+import { activitySchema } from "../../../lib/schemas/activitySchema";
+import { zodResolver } from '@hookform/resolvers/zod';
  
 export default function ActivityForm() {
-    const { register, reset, handleSubmit } = useForm();
+    const { register, reset, handleSubmit, formState: {errors} } = useForm<ActivitySchema>({
+        resolver: zodResolver(activitySchema)
+    });
     const {id} = useParams();
     const {createActivity, updateActivity, activity, isLoadingActivity} = useActivities(id);
 
@@ -26,7 +31,7 @@ export default function ActivityForm() {
     <Paper sx={{borderRadius: 3, padding: 3}}>
         <Typography variant="h5" gutterBottom color="primary">{activity ? 'Edit Activity' : 'Create Activity'}</Typography>
         <Box component="form" onSubmit={handleSubmit(onSubmit)} display="flex" flexDirection="column" gap={3}>
-            <TextField {...register('title')} label="Title" defaultValue={activity?.title} />
+            <TextField {...register('title')} label="Title" defaultValue={activity?.title} error={!!errors.title} helperText={errors.title?.message}/>
             <TextField {...register('description')} label="Description" defaultValue={activity?.description} multiline rows={3} />
             <TextField {...register('category')} label="Category" defaultValue={activity?.category} />
             <TextField {...register('date')} label="Date" type="date"
