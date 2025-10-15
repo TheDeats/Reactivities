156a51dfa6d126f0e539bc01a8b2bf4a268f9864
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-14 20:59:58 -0700

    Added a TestError component that has a bunch of buttons to fire endpoint requests that result in various errors. Added a menu item to the navbar to get to the Error Test page and added a route for it. Created the BuggyController to handle incoming error requests. Updated the axios response interceptor so it actually handles the errors that come through, the try catch did not catch the errors. Added react-toastify package and firing toast messages when the various errors come through. Added the ToastController to our main component.

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -5,6 +5,7 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetailPage from "../../features/activities/details/ActivityDetailPage";
 import Counter from "../../features/coutner/Counter";
+import TestErrors from "../../features/errors/TestErrors";
 
 
 export const router = createBrowserRouter([
@@ -18,6 +19,7 @@ export const router = createBrowserRouter([
             {path: 'createActivity', element: <ActivityForm key='create'/>},
             {path: 'manage/:id', element: <ActivityForm />},
             {path: 'counter', element: <Counter />},
+            {path: 'errors', element: <TestErrors />}
         ]
     }
 ])
