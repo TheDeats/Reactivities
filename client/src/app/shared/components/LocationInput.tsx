5ce7f71930a6a563a297b0565ff69720e8adf7fe
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-16 16:58:39 -0700

    Created the LocationInput component for autocompletion in the venue field using LocationIQ. Added LocationIQ Suggestion and Address types to the index type definition file. Updated the activitySchema for validation to validation a location with the city, venue, latitude and longitude values. Also updated the export to use input so types are defined and not set to undefined. Fixed type definition errors in the ActivityForm and added the LocationInput component instead of city and venue.

---
+++ client/src/app/shared/components/LocationInput.tsx
@@ -0,0 +1,87 @@
+import { useEffect, useMemo, useState } from "react";
+import { type FieldValues, useController, type UseControllerProps } from "react-hook-form"
+import { type LocationIQSuggestion } from "../../../lib/types";
+import { Box, debounce, List, ListItemButton, TextField, Typography } from "@mui/material";
+import axios from "axios";
+
+type Props<T extends FieldValues> = {
+    label: string
+} & UseControllerProps<T>
+
+export default function LocationInput<T extends FieldValues>(props: Props<T>) {
+    const {field, fieldState} = useController({...props});
+    const [loading, setLoading] = useState(false);
+    const [suggestiongs, setSuggestions] = useState<LocationIQSuggestion[]> ([]);
+    const [inputValue, setInputValue] = useState(field.value || '');
+
+    const locationUrl = 'https://api.locationiq.com/v1/autocomplete?key=pk.7fb26b1905df3d883c55065bec529139&limit=5&dedupe=1&';
+
+    useEffect(() => {
+        if (field.value && typeof field.value === 'object'){
+            setInputValue(field.value.venue || '');
+        } else {
+            setInputValue(field.value || '');
+        }
+    }, [field.value])
+
+    const fetchSuggestions = useMemo(
+        () => debounce(async (query: string) => {
+            if (!query || query.length < 3){
+                setSuggestions([]);
+                return;
+            }
+            setLoading(true);
+            try {
+                const res = await axios.get<LocationIQSuggestion[]>(`${locationUrl}q=${query}`)
+                setSuggestions(res.data);
+            } catch (error) {
+                console.log(error);
+            } finally{
+                setLoading(false);
+            }
+        }, 500), [locationUrl]
+    );
+
+    const handleChange = async (value: string) => {
+        field.onChange(value);
+        await fetchSuggestions(value);
+    }
+
+    const handleSelect = (location: LocationIQSuggestion) => {
+        const city = location.address?.city || location.address?.town || location.address?.village;
+        const venue = location.display_name;
+        const latitude = location.lat;
+        const longitude = location.lon;
+        setInputValue(venue);
+        field.onChange({city, venue, latitude, longitude});
+        setSuggestions([]);
+    }
+    
+  return (
+    <Box>
+        <TextField
+            {...props}
+            value={inputValue}
+            onChange={e => handleChange(e.target.value)}
+            fullWidth
+            variant="outlined"
+            error={!!fieldState.error}
+            helperText={fieldState.error?.message}
+        />
+        {loading && <Typography>Loading...</Typography>}
+        {suggestiongs.length > 0 && (
+            <List sx={{border: 1}}>
+                {suggestiongs.map(suggestion => (
+                    <ListItemButton
+                        divider
+                        key={suggestion.place_id}
+                        onClick={() => handleSelect(suggestion)}>
+                        {suggestion.display_name}
+                    </ListItemButton>
+                ))}
+
+            </List>
+        )}
+    </Box>
+  )
+}
--- client/src/lib/schemas/activitySchema.ts
+++ client/src/lib/schemas/activitySchema.ts
@@ -6,11 +6,15 @@ export const activitySchema = z.object({
     title: requiredString('Title'),
     description: requiredString('Description'),
     category: requiredString('Category'),
-    date: z.coerce.date<Date>({
+    date: z.coerce.date({
         message: 'Date is required'
     }),
-    city: requiredString('City'),
-    venue: requiredString('Venue'),
+    location: z.object({
+        venue: requiredString('Venue'),
+        city: z.string().optional(),
+        latitude: z.coerce.number(),
+        longitude: z.coerce.number()
+    })
 })
 
-export type ActivitySchema = z.infer<typeof activitySchema>;
+export type ActivitySchema = z.input<typeof activitySchema>;
