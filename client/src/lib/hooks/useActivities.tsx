c0f3786faa0a4394d263355ab8fc6a19615209b8
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-20 10:23:05 -0700

    Added a function to update a users attendance to an activity to the useActivities hook. Updated the ActivityDetailsHeader to use the new function and added some functionality to disable the buttons if the update is pending. Passing in the activity.id to the useActivities hook so it will have the id and can invalidate the query without error. Removed unnecessary import from ActivityDetailsInfo. Added a key to the ActivityDetailsSideBar Grid per requirements and to remove a warning.

--- client/src/lib/hooks/useActivities.tsx
+++ client/src/lib/hooks/useActivities.tsx
@@ -76,7 +76,25 @@ export const useActivities = (id?: string) => {
         }
     })
 
+    const updateAttendance = useMutation({
+        mutationFn: async (id: string) => {
+            await agent.post(`/activities/${id}/attend`)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities', id]
+            })
+        }
+    })
+
     return {
-        activities, isLoading, updateActivity, createActivity, deleteActivity, activity, isLoadingActivity
+        activities, 
+        isLoading,
+        updateActivity, 
+        createActivity, 
+        deleteActivity, 
+        activity, 
+        isLoadingActivity, 
+        updateAttendance
     }
 }
