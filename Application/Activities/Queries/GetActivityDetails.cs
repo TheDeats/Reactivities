f23ce5eb185d8ab1d5f2a3767c7462950831eae4
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-19 18:44:28 -0700

    Updated GetActivityDetails and GetActivityList to use projection to only get the required data back from the database to populate the ActivityDto. Updated type in ActivitiesController.

--- Application/Activities/Queries/GetActivityDetails.cs
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -1,6 +1,7 @@
 using Application.Activities.DTOs;
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -19,8 +20,7 @@ public class GetActivityDetails
         public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
         {
             var activity = await context.Activities
-                .Include(x => x.Attendees)
-                .ThenInclude(x => x.User)
+                .ProjectTo<ActivityDto>(mapper.ConfigurationProvider)
                 .FirstOrDefaultAsync(x => request.Id == x.Id, cancellationToken);
 
             if (activity == null)
@@ -28,7 +28,7 @@ public class GetActivityDetails
                 return Result<ActivityDto>.Failure("Activity not found", 404);
             }
 
-            return Result<ActivityDto>.Success(mapper.Map<ActivityDto>(activity));
+            return Result<ActivityDto>.Success(activity);
         }
     }
 }
