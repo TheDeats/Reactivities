c39859fce8baf7545bc9cd32d6f32a82afc8ef21
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-21 15:18:08 -0700

    Created a Query to get all the photos of a user. Added a method to the controller to handle http requests to get all the photos for a user. Tested add photo and get photos methods in postman successfully.

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,4 +1,5 @@
 using Application.Profiles.Commands;
+using Application.Profiles.Queries;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,12 @@ public class ProfilesController : BaseApiController
     [HttpPost("add-photo")]
     public async Task<ActionResult<Photo>> AddPhoto(IFormFile file)
     {
-        return HandleResult(await Mediator.Send(new AddPhoto.Command{File = file}));
+        return HandleResult(await Mediator.Send(new AddPhoto.Command { File = file }));
+    }
+
+    [HttpGet("{userId}/photos")]
+    public async Task<ActionResult<List<Photo>>> GetPhotosForUser(string userId)
+    {
+        return HandleResult(await Mediator.Send(new GetProfilePhotos.Query { UserId = userId }));
     }
 }
