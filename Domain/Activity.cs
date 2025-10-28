583f648bc93adff9f3122c321cad6eefd1c29db4
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-28 14:11:06 -0700

    Getting ready to add a new Comments feature for an activity. Created a Comment class. Added the navigation prop for the colleciton of comments to the Activity class. Added a DbSet for the Comments table that will be created. Added the migration.

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -17,5 +17,6 @@ public class Activity
 
     // navigation props
     public ICollection<ActivityAttendee> Attendees { get; set; } = [];
+    public ICollection<Comment> Comments { get; set; } = [];
 
 }
