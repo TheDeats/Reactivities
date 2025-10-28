f3364cde69bfc12ee438dc2e51d63efe94817b07
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-28 14:45:03 -0700

    Created a AddComment command handler to add comments to the database. Updated the comment class so we don't have to include the objects for the nav props when creating a new comment. No controller will be added, http requests will be handled by SignalR.

--- Domain/Comment.cs
+++ Domain/Comment.cs
@@ -8,9 +8,9 @@ public class Comment
 
     // Nav props
     public required string UserId { get; set; }
-    public required User User { get; set; }
+    public User User { get; set; } = null!;
 
     public required string ActivityId { get; set; }
-    public required Activity Activity { get; set; }
+    public Activity Activity { get; set; } = null!;
 
 }
