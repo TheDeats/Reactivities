404d8d7e400e3c577d2515546ee3fb3522752d52
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-28 23:40:16 -0700

    Created a profile query to either get all the users that the user follows or get all the users that follow the user based on the predicate passed in. Created a http request handler in the ProfilesController for the new query.

---
+++ Application/Profiles/Queries/GetFollowings.cs
@@ -0,0 +1,43 @@
+using Application.Core;
+using Application.Profiles.DTOs;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Profiles.Queries;
+
+public class GetFollowings
+{
+    public class Query : IRequest<Result<List<UserProfile>>>
+    {
+        public string Predicate { get; set; } = "followers";
+        public required string UserId { get; set; }
+    }
+
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Query, Result<List<UserProfile>>>
+    {
+        public async Task<Result<List<UserProfile>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            List<UserProfile> profiles = [];
+            switch (request.Predicate)
+            {
+                case "followers":
+                    profiles = await context.UserFollowings.Where(x => x.TargetId == request.UserId)
+                                    .Select(x => x.Observer)
+                                    .ProjectTo<UserProfile>(mapper.ConfigurationProvider)
+                                    .ToListAsync(cancellationToken);
+                    break;
+                case "followings":
+                    profiles = await context.UserFollowings.Where(x => x.ObserverId == request.UserId)
+                                    .Select(x => x.Target)
+                                    .ProjectTo<UserProfile>(mapper.ConfigurationProvider)
+                                    .ToListAsync(cancellationToken);
+                    break;
+            }
+
+            return Result<List<UserProfile>>.Success(profiles);
+        }
+    }
+}
