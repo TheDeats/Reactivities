4f595eafb5899bb6d7c45c63216cc1bd5fbde9eb
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-30 11:52:01 -0700

    Updated the UserProfile DTO with some additonal properties to support the following feature. Updating the mapping profiles to populate the new properties and added a string to pass in the user Id. Updated the ProjectTo method in GetActivityDetails, GetActivityList, GetFollowing and GetProfile to pass in the id for the mapping profile so we can populate the newly added properties.

--- Application/Profiles/DTOs/UserProfile.cs
+++ Application/Profiles/DTOs/UserProfile.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Application.Profiles.DTOs;
 
 public class UserProfile
@@ -8,4 +6,7 @@ public class UserProfile
     public required string Id { get; set; }
     public string? Bio { get; set; }
     public string? ImageUrl { get; set; }
+    public bool Following { get; set; }
+    public int FollowersCount { get; set; }
+    public int FollowingCount { get; set; }
 }
