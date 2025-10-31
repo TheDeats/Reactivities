41849a33e86b3091eafc81cd1f35afeb53f3b512
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-10-30 22:51:55 -0700

    Updated the Profile type in the index.d.ts typescript definition file to match our updated UserProfile. The API gets a User which is projected to a UserProfile using the Mapping Profile. The controller returns a JSON response and the useProfile query is expecting a Profile type. Property names are converted to camel case when serialized to JSON. Updated the AvatarPopover, ActivityDetailsSidebar, ProfileCard and ProfileHeader to use the new properties instead of placeholder values.

--- client/src/features/profiles/ProfileHeader.tsx
+++ client/src/features/profiles/ProfileHeader.tsx
@@ -5,7 +5,6 @@ type Props = {
 }
 
 export default function ProfileHeader({profile}: Props) {
-    const isFollowing = true;
 
   return (
     <Paper elevation={3} sx={{p:4, borderRadius: 3}}> 
@@ -15,7 +14,7 @@ export default function ProfileHeader({profile}: Props) {
                     <Avatar src={profile.imageUrl} alt={profile.displayName + ' image'} sx={{width: 150, height: 150}}/>
                     <Box display='flex' flexDirection='column' gap={2}>
                         <Typography variant="h4">{profile.displayName}</Typography>
-                        {isFollowing && 
+                        {profile.following && 
                             <Chip variant="outlined" color="secondary" label='Following' sx={{borderRadius: 1}}/>}
                     </Box>
                 </Stack>
@@ -25,16 +24,16 @@ export default function ProfileHeader({profile}: Props) {
                     <Box display='flex' justifyContent='space-around' width='100%'>
                         <Box textAlign='center'>
                             <Typography variant="h6">Followers</Typography>
-                            <Typography variant="h3">5</Typography>
+                            <Typography variant="h3">{profile.followersCount}</Typography>
                         </Box>
                         <Box textAlign='center'>
                             <Typography variant="h6">Following</Typography>
-                            <Typography variant="h3">42</Typography>
+                            <Typography variant="h3">{profile.followingCount}</Typography>
                         </Box>
                     </Box>
                     <Divider sx={{width: '100%'}}/>
-                    <Button fullWidth variant="outlined" color={isFollowing ? 'error' : 'success'}>
-                        {isFollowing ? 'Unfollow' : 'Follow'}
+                    <Button fullWidth variant="outlined" color={profile.following ? 'error' : 'success'}>
+                        {profile.following ? 'Unfollow' : 'Follow'}
                     </Button>
                 </Stack>
             </Grid2>
