b3985f55e275f436d49dec9d0537d85308c59e86
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-11-01 23:47:00 -0700

    Added the react-intersection-observer package to implement infinite scrolling in the activities list. Added a useEffect to trigger additional loads and changed the Fragment to a Box which takes the ref property. Removed the load button from the ActivityDashboard. It is working but it seems to load more activities than I would expect. However it does load a certain amount and stop until I scroll down.

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,23 +1,12 @@
-import { Button, Grid2 } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityFilters from "./ActivityFilters";
-import { useActivities } from "../../../lib/hooks/useActivities";
 
-export default function ActivityDashboard() {
-  const { isFetchingNextPage, fetchNextPage, hasNextPage} = useActivities();
-  
+export default function ActivityDashboard() { 
   return (
     <Grid2 container spacing={3}>
         <Grid2 size={8}>
             <ActivityList />
-            <Button
-              onClick={() => fetchNextPage()}
-              sx={{my: 2, float: 'right'}}
-              variant="contained"
-              disabled={!hasNextPage || isFetchingNextPage}
-            >
-              Load More
-            </Button>
         </Grid2>
         <Grid2 
           size={4}
