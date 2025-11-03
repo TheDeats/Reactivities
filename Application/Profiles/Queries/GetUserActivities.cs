c8073ad1890dcde3bc749b85ef40e4b78d3c493a
DEATSPC\JDeat <JDeaton26@gmail.com>
2025-11-03 00:12:39 -0800

    Working on the backend to populate the events a user is following on their profile page. Created the UserActivityDto class to get only the required details for an activity we want to display. Created a query request handler that gets all the activites from either the past, future or the ones their hosting. Created a mapping profile from an Activity to an UserActivityDto. Added a http request handler in the ProfilesController that takes a filter for either past, future, or hosting events.

---
+++ Application/Profiles/Queries/GetUserActivities.cs
@@ -0,0 +1,42 @@
+using Application.Core;
+using Application.Profiles.DTOs;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Profiles.Queries;
+
+public class GetUserActivities
+{
+    public class Query : IRequest<Result<List<UserActivityDto>>>
+    {
+        public required string Filter { get; set; }
+        public required string UserId { get; set; }
+    }
+
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Query, Result<List<UserActivityDto>>>
+    {
+        public async Task<Result<List<UserActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            IQueryable<Activity>? query = context.ActivityAttendees
+                .Where( u => u.User.Id == request.UserId)
+                .OrderBy(x => x.Activity.Date)
+                .Select(x => x.Activity)
+                .AsQueryable();
+
+            query = request.Filter switch
+            {
+                "past" => query.Where(x => x.Date < DateTime.UtcNow),
+                "hosting" => query.Where(x => x.Attendees.Any(a => a.IsHost)),
+                _ => query.Where(x => x.Date >= DateTime.UtcNow),
+            };
+
+            IQueryable<UserActivityDto>? projectedActivities = query.ProjectTo<UserActivityDto>(mapper.ConfigurationProvider);
+            List<UserActivityDto>? activities = await projectedActivities.ToListAsync(cancellationToken);
+            return Result<List<UserActivityDto>>.Success(activities);
+        }
+    }
+}
